52a1b56f666f916c53d2cc28775fa8ad147fabe8
robert <ich2211@googlemail.com>
2021-05-21 23:27:08 +0200

    updated test-structure

--- app/src/androidTest/java/net/usemyskills/grasp/EntityReadWriteTest.java
+++ app/src/androidTest/java/net/usemyskills/grasp/EntityReadWriteTest.java
@@ -22,7 +22,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
-import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.concurrent.CountDownLatch;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -52,55 +53,60 @@ public class EntityReadWriteTest {
 
     @Test
     public void writeDataTagAndReadAll() throws Exception {
-        dataTagDao.insert(new DataTag(3,"test", "testdescription"));
+        final CountDownLatch signal = new CountDownLatch(1);
         dataTagDao.getAll().observeForever(dataTags -> {
             assertThat(dataTags.size(), equalTo(1));
             DataTag singleDataTag = dataTags.get(0);
             assertThat(singleDataTag.getName(), equalTo("test"));
-            assertThat(singleDataTag.getTagId(), equalTo(3));
+            assertThat(singleDataTag.getId(), equalTo(3));
             assertThat(singleDataTag.getDescription(), equalTo("testdescription"));
+            signal.countDown();
         });
+        dataTagDao.insert(new DataTag(3,"test", "testdescription"));
+        signal.await();
     }
 
     @Test
     public void writeDataTagWithoutIdAndReadAll() throws Exception {
-        dataTagDao.insert(new DataTag("test", "testdescription"));
+        final CountDownLatch signal = new CountDownLatch(1);
         dataTagDao.getAll().observeForever(dataTags -> {
             assertThat(dataTags.size(), equalTo(1));
             DataTag singleDataTag = dataTags.get(0);
             assertThat(singleDataTag.getName(), equalTo("test"));
-            assertThat(singleDataTag.getTagId(), equalTo(1));
+            assertThat(singleDataTag.getId(), equalTo(1));
             assertThat(singleDataTag.getDescription(), equalTo("testdescription"));
+            signal.countDown();
         });
+        dataTagDao.insert(new DataTag("test", "testdescription"));
+        signal.await();
     }
 
     @Test
     public void writeDataTypeTagAndReadAll() throws Exception {
-        dataTypeDao.insert(new DataType(7,"test", "testdescription", 60, "min"));
         dataTypeDao.getAll().observeForever(dataTypeTags -> {
             assertThat(dataTypeTags.size(), equalTo(1));
             DataType singleDataType = dataTypeTags.get(0);
             assertThat(singleDataType.getName(), equalTo("test"));
-            assertThat(singleDataType.getTagId(), equalTo(7));
+            assertThat(singleDataType.getId(), equalTo(7));
             assertThat(singleDataType.getDescription(), equalTo("testdescription"));
-            assertThat(singleDataType.getModifier(), equalTo(60));
             assertThat(singleDataType.getUnit(), equalTo("min"));
         });
+        dataTypeDao.insert(new DataType(7,"test", "testdescription", "min"));
     }
 
     @Test
     public void writeDataAndReadAllDataContainer() throws Exception {
-        dataTypeDao.insert(new DataType(2,"Aphrodite", "", 60, "min"));
-        dataTagDao.insert(new DataTag(1,"High"));
-        dataDao.insert(new Data(1,2,1, new Date(2020-1900,11-1,22), 90));
         dataContainerDao.getAll().observeForever(dataContainers -> {
             assertThat(dataContainers.size(), equalTo(1));
             DataContainer singleDataContainer = dataContainers.get(0);
             assertThat(singleDataContainer.getTypeName(), equalTo("Aphrodite"));
             assertThat(singleDataContainer.getTagName(), equalTo("High"));
             assertThat(singleDataContainer.getDateString(), equalTo("22.11.2020"));
-            assertThat(singleDataContainer.getValue(), equalTo(1.5));
+            assertThat(singleDataContainer.getValue(), equalTo(90));
         });
+        dataTypeDao.insert(new DataType(2,"Aphrodite", "", "min"));
+        dataTagDao.insert(new DataTag(1,"High", ""));
+        dataDao.insert(new Data(1,2,1, new GregorianCalendar(2020-1900,11-1,22).getTime(), 90));
 
     }
 }
