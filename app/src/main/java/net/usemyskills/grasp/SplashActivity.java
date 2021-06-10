46f51db9844713628e602f77a883aab107a5ca10
robert <ich2211@googlemail.com>
2021-06-10 14:08:21 +0200

    Issue-9: preloader added

---
+++ app/src/main/java/net/usemyskills/grasp/SplashActivity.java
@@ -0,0 +1,39 @@
+package net.usemyskills.grasp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import net.usemyskills.grasp.databinding.ActivitySplashBinding;
+import net.usemyskills.grasp.viewmodel.RecordGroupViewModel;
+import net.usemyskills.grasp.viewmodel.RecordViewModel;
+import net.usemyskills.grasp.viewmodel.TagViewModel;
+
+public class SplashActivity extends AppCompatActivity {
+
+    /** Called when the activity is first created. */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivitySplashBinding binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        /// init all viewModels
+        ViewModelProvider viewModelProvider = new ViewModelProvider(this);
+        viewModelProvider.get(TagViewModel.class);
+        viewModelProvider.get(RecordGroupViewModel.class);
+        viewModelProvider.get(RecordViewModel.class);
+
+        int SPLASH_DISPLAY_LENGTH = 1000;
+        new Handler().postDelayed(() -> {
+            Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+            this.startActivity(mainIntent);
+            this.finish();
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+
+}
